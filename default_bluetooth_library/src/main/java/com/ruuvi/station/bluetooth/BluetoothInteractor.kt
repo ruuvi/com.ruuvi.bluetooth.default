00251cb04e991cc102e6280e3822110b525dca92
Denis Andreev <andreev.denis@gmail.com>
2021-02-24 02:24:09 +0300

    ISSUE #237 More datapoints than was requested by settings

--- default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/BluetoothInteractor.kt
+++ default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/BluetoothInteractor.kt
@@ -57,11 +57,7 @@ class BluetoothInteractor(
     }
 
     init {
-
         Foreground.init(application)
-
-        // foreground listener does not trigger the first onforeground, so first just set it to true
-        isRunningInForeground = true
 
         Foreground.get().addListener(listener)
     }
@@ -94,7 +90,7 @@ class BluetoothInteractor(
     }
 
     fun stopScanningFromBackground() {
-        Timber.d("stopScanningFromBackground")
+        Timber.d("stopScanningFromBackground (isRunningInForeground = $isRunningInForeground)")
         if (!isRunningInForeground) ruuviRangeNotifier.stopScanning()
     }
 

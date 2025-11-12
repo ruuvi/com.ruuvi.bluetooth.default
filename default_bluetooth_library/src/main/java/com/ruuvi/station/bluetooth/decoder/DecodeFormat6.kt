6fda6207f8d2df2fe4acaca16a0a49280bc5d3f1
Denis Andreev <andreev.denis@gmail.com>
2025-11-12 16:05:13 +0300

    Fix validation for dataformat E1 and 6

--- default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/decoder/DecodeFormat6.kt
+++ default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/decoder/DecodeFormat6.kt
@@ -37,7 +37,7 @@ class DecodeFormat6: RuuviTagDecoder {
         result.dBaAvg = ((((data[DBA_AVG_POSITION + offset].toInt() and 0xFF) shl 1) or if (dbaAvgFlag) 1 else 0) / 5.0 + 18).roundHalfUp(2)
         result.measurementSequenceNumber = data[SEQUENCE_POSITION + offset].toInt() and 0xFF
 
-        result = validateValues(result)
+        result = validateValues6(result)
         Timber.d("DecodeFormat${DATA_FORMAT} $result from bytes [${data.joinToString(", ") { (it.toInt() and 0xff).toString() }}]")
         return result
     }
@@ -52,6 +52,88 @@ class DecodeFormat6: RuuviTagDecoder {
         return (byte.toInt() shr bitIndex and 1) == 1
     }
 
+    fun validateValues6(sensor: FoundRuuviTag) : FoundRuuviTag {
+        sensor.temperature?.let {
+            if (it !in TEMPERATURE_MINIMUM..TEMPERATURE_MAXIMUM) sensor.temperature = null
+        }
+
+        sensor.humidity?.let {
+            if (it !in HUMIDITY_MINIMUM..HUMIDITY_MAXIMUM) sensor.humidity = null
+        }
+
+        sensor.pressure?.let {
+            if (it !in PRESSURE_MINIMUM..PRESSURE_MAXIMUM) sensor.pressure = null
+        }
+
+        sensor.accelX?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelX = null
+        }
+
+        sensor.accelY?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelY = null
+        }
+
+        sensor.accelZ?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelZ = null
+        }
+
+        sensor.txPower?.let {
+            if (it !in TX_POWER_MINIMUM..TX_POWER_MAXIMUM) sensor.txPower = null
+        }
+
+        sensor.voltage?.let {
+            if (it !in VOLTAGE_MINIMUM..VOLTAGE_MAXIMUM) sensor.voltage = null
+        }
+
+        sensor.movementCounter?.let {
+            if (it !in MOVEMENT_MINIMUM..MOVEMENT_MAXIMUM) sensor.movementCounter = null
+        }
+
+        sensor.measurementSequenceNumber?.let {
+            if (it !in MEASUREMENT_SEQUENCE_MINIMUM..MEASUREMENT_SEQUENCE_MAXIMUM) sensor.measurementSequenceNumber = null
+        }
+
+        sensor.pm1?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm1 = null
+        }
+
+        sensor.pm25?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm25 = null
+        }
+
+        sensor.pm4?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm4 = null
+        }
+
+        sensor.pm10?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm10 = null
+        }
+
+        sensor.co2?.let {
+            if (it !in CO2_MINIMUM..CO2_MAXIMUM) sensor.co2 = null
+        }
+
+        sensor.voc?.let {
+            if (it !in VOC_MINIMUM..VOC_MAXIMUM) sensor.voc = null
+        }
+
+        sensor.nox?.let {
+            if (it !in NOX_MINIMUM..NOX_MAXIMUM) sensor.nox = null
+        }
+
+        sensor.luminosity?.let {
+            if (it !in LUMINOSITY_MINIMUM..LUMINOSITY_MAXIMUM) sensor.luminosity = null
+        }
+
+        sensor.dBaAvg?.let {
+            if (it !in DBA_MINIMUM..DBA_MAXIMUM) sensor.dBaAvg = null
+        }
+
+        sensor.dBaPeak?.let {
+            if (it !in DBA_MINIMUM..DBA_MAXIMUM) sensor.dBaPeak = null
+        }
+        return sensor
+    }
     companion object {
         const val DATA_FORMAT = 0x06
         const val TEMPERATURE_POSITION = 1
@@ -66,5 +148,47 @@ class DecodeFormat6: RuuviTagDecoder {
         const val SEQUENCE_POSITION = 15
         const val FLAGS_POSITION = 16
         const val LUMINOSITY_DECODE_BASE = 65535.0
+
+        const val TEMPERATURE_MINIMUM = -163.835
+        const val TEMPERATURE_MAXIMUM = 163.835
+
+        const val HUMIDITY_MINIMUM = 0.0
+        const val HUMIDITY_MAXIMUM = 100.0
+
+        const val PRESSURE_MINIMUM = 50000.0
+        const val PRESSURE_MAXIMUM = 115534.0
+
+        const val ACCELERATION_MINIMUM = -32.767
+        const val ACCELERATION_MAXIMUM = 32.767
+
+        const val TX_POWER_MINIMUM = -40.0
+        const val TX_POWER_MAXIMUM = 20.0
+
+        const val VOLTAGE_MINIMUM = 1.600
+        const val VOLTAGE_MAXIMUM = 7.620
+
+        const val MOVEMENT_MINIMUM = 0
+        const val MOVEMENT_MAXIMUM = 254
+
+        const val MEASUREMENT_SEQUENCE_MINIMUM = 0
+        const val MEASUREMENT_SEQUENCE_MAXIMUM = 65534
+
+        const val PM_MINIMUM = 0.0
+        const val PM_MAXIMUM = 6553.4
+
+        const val CO2_MINIMUM = 0
+        const val CO2_MAXIMUM = 40000
+
+        const val VOC_MINIMUM = 1
+        const val VOC_MAXIMUM = 500
+
+        const val NOX_MINIMUM = 1
+        const val NOX_MAXIMUM = 500
+
+        const val LUMINOSITY_MINIMUM = 0.0
+        const val LUMINOSITY_MAXIMUM = 65535.0
+
+        const val DBA_MINIMUM = 0.0
+        const val DBA_MAXIMUM = 120.0
     }
 }
--- default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/decoder/DecodeFormatE1.kt
+++ default_bluetooth_library/src/main/java/com/ruuvi/station/bluetooth/decoder/DecodeFormatE1.kt
@@ -44,7 +44,7 @@ class DecodeFormatE1: RuuviTagDecoder {
                 (data[SEQUENCE_POSITION + 2 + offset].toInt() and 0xFF)
         result.voltage = null
 
-        result = validateValues(result)
+        result = validateValuesE1(result)
 
         Timber.d("DecodeFormat$DATA_FORMAT $result from bytes [${data.joinToString(", ") { (it.toInt() and 0xff).toString() }}]")
         return result
@@ -53,6 +53,89 @@ class DecodeFormatE1: RuuviTagDecoder {
     fun isBitSet(byte: Byte, bitIndex: Int): Boolean {
         require(bitIndex in 0..7) { "bitIndex must be in 0..7" }
         return (byte.toInt() shr bitIndex and 1) == 1
+    }
+
+    fun validateValuesE1(sensor: FoundRuuviTag) : FoundRuuviTag {
+        sensor.temperature?.let {
+            if (it !in TEMPERATURE_MINIMUM..TEMPERATURE_MAXIMUM) sensor.temperature = null
+        }
+
+        sensor.humidity?.let {
+            if (it !in HUMIDITY_MINIMUM..HUMIDITY_MAXIMUM) sensor.humidity = null
+        }
+
+        sensor.pressure?.let {
+            if (it !in PRESSURE_MINIMUM..PRESSURE_MAXIMUM) sensor.pressure = null
+        }
+
+        sensor.accelX?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelX = null
+        }
+
+        sensor.accelY?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelY = null
+        }
+
+        sensor.accelZ?.let {
+            if (it !in ACCELERATION_MINIMUM..ACCELERATION_MAXIMUM) sensor.accelZ = null
+        }
+
+        sensor.txPower?.let {
+            if (it !in TX_POWER_MINIMUM..TX_POWER_MAXIMUM) sensor.txPower = null
+        }
+
+        sensor.voltage?.let {
+            if (it !in VOLTAGE_MINIMUM..VOLTAGE_MAXIMUM) sensor.voltage = null
+        }
+
+        sensor.movementCounter?.let {
+            if (it !in MOVEMENT_MINIMUM..MOVEMENT_MAXIMUM) sensor.movementCounter = null
+        }
+
+        sensor.measurementSequenceNumber?.let {
+            if (it !in MEASUREMENT_SEQUENCE_MINIMUM..MEASUREMENT_SEQUENCE_MAXIMUM) sensor.measurementSequenceNumber = null
+        }
+
+        sensor.pm1?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm1 = null
+        }
+
+        sensor.pm25?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm25 = null
+        }
+
+        sensor.pm4?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm4 = null
+        }
+
+        sensor.pm10?.let {
+            if (it !in PM_MINIMUM..PM_MAXIMUM) sensor.pm10 = null
+        }
+
+        sensor.co2?.let {
+            if (it !in CO2_MINIMUM..CO2_MAXIMUM) sensor.co2 = null
+        }
+
+        sensor.voc?.let {
+            if (it !in VOC_MINIMUM..VOC_MAXIMUM) sensor.voc = null
+        }
+
+        sensor.nox?.let {
+            if (it !in NOX_MINIMUM..NOX_MAXIMUM) sensor.nox = null
+        }
+
+        sensor.luminosity?.let {
+            if (it !in LUMINOSITY_MINIMUM..LUMINOSITY_MAXIMUM) sensor.luminosity = null
+        }
+
+        sensor.dBaAvg?.let {
+            if (it !in DBA_MINIMUM..DBA_MAXIMUM) sensor.dBaAvg = null
+        }
+
+        sensor.dBaPeak?.let {
+            if (it !in DBA_MINIMUM..DBA_MAXIMUM) sensor.dBaPeak = null
+        }
+        return sensor
     }
 
     companion object {
@@ -73,5 +156,47 @@ class DecodeFormatE1: RuuviTagDecoder {
         const val DBA_PEAK_POSITION = 24
         const val SEQUENCE_POSITION = 25
         const val FLAGS_POSITION = 28
+
+        const val TEMPERATURE_MINIMUM = -163.835
+        const val TEMPERATURE_MAXIMUM = 163.835
+
+        const val HUMIDITY_MINIMUM = 0.0
+        const val HUMIDITY_MAXIMUM = 100.0
+
+        const val PRESSURE_MINIMUM = 50000.0
+        const val PRESSURE_MAXIMUM = 115534.0
+
+        const val ACCELERATION_MINIMUM = -32.767
+        const val ACCELERATION_MAXIMUM = 32.767
+
+        const val TX_POWER_MINIMUM = -40.0
+        const val TX_POWER_MAXIMUM = 20.0
+
+        const val VOLTAGE_MINIMUM = 1.600
+        const val VOLTAGE_MAXIMUM = 7.620
+
+        const val MOVEMENT_MINIMUM = 0
+        const val MOVEMENT_MAXIMUM = 254
+
+        const val MEASUREMENT_SEQUENCE_MINIMUM = 0
+        const val MEASUREMENT_SEQUENCE_MAXIMUM = 65534
+
+        const val PM_MINIMUM = 0.0
+        const val PM_MAXIMUM = 6553.4
+
+        const val CO2_MINIMUM = 0
+        const val CO2_MAXIMUM = 40000
+
+        const val VOC_MINIMUM = 1
+        const val VOC_MAXIMUM = 500
+
+        const val NOX_MINIMUM = 1
+        const val NOX_MAXIMUM = 500
+
+        const val LUMINOSITY_MINIMUM = 0.0
+        const val LUMINOSITY_MAXIMUM = 167772.14
+
+        const val DBA_MINIMUM = 0.0
+        const val DBA_MAXIMUM = 120.0
     }
 }
